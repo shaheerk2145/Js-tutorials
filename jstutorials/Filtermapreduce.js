623b517cb419f5d5feb33852caa28349d9ab52e9
shaheer <shaheerkhan2145@github.com>
2024-09-08 11:07:15 +0500

    js tutorials

--- jstutorials/Filtermapreduce.js
+++ jstutorials/Filtermapreduce.js
@@ -1,3 +1,4 @@
+//Foreach
 const lang = ["cpp","py","rb","js","java","xml"];
 // lang.forEach((item) => {
 //     console.log(item);
@@ -8,7 +9,7 @@ const values = lang.forEach((item) => {
 });
 // console.log(values); //undefined
 
-
+//Filter
 
 const digits=[1,2,3,4,5,6,7,8,9,10];
 // const reult = digits.filter((num)=>{
@@ -49,6 +50,57 @@ const books = [
 // let myBook=books.filter((kitab)=>{
 //     return kitab.genre === 'Science' && kitab.publish<=2000;
 // })
-console.log(myBook);
+// console.log(myBook);
+
+
+// map
+const figures = [10.2,11.3,4.3,4.8,7.5,3.1,10.0,9,5];
+// const tFigures = figures.map((num)=>num + 10);
+// chaining
+// const tFigures = figures
+//         .map((num)=>{ return num *5})
+//         .map((num)=>{ return num -1})
+//         .filter((num)=>{ return num >= 25})
+// console.log(tFigures);
+
+
+
+
+// reduce======================================================================
+
+const runs =[11,17,3,12,19,41,32,27,9,13,12]
+const total = runs.reduce(function(accumulator,currentValue){
+    // console.log(`player number  scores ${currentValue}`);
+    return accumulator + currentValue;
+},0);
+// console.log(total);
+const myTotal = runs.reduce( (acc, curr) => acc+curr, 0);
+// console.log(myTotal);
+
+
+
+const shoppingCart = [
+    {
+        itemName: "js course ",
+        price: 2999
+    },
+    {
+        itemName: " py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course ",
+        price: 12999
+    },
+]
+const pesayDena = shoppingCart.reduce((acc,currVal)=>{ 
+    console.log(`course ${currVal.itemName} price is ${currVal.price}`);
+    return acc+ currVal.price 
+},0);
+console.log(`Total Price : ${pesayDena}`);
 
 
